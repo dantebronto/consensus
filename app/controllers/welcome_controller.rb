f5f00d3af6bc947a2ba6885bd6fec1a3fbba8542
Kellen Presley <kellenpresley@kellen-presleys-macbook-pro.local>
2009-08-23 13:25:41 -0400

    about page added

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,6 @@
 class WelcomeController < ApplicationController
-  before_filter :login_required, :except => [:index]
+  before_filter :login_required, :except => [:index, :about]
 
-  def index
-  end
-  
+  def index; end
+  def about; end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :users, :except => [:show]
   map.resources :sessions, :only => [:new, :create, :destroy]
   map.resources :votes, :except => [:edit], :member => { :prioritize => :get, :allocate => :get, :cast => :get }
-  map.login "/login", :controller => "sessions", :action => "new"
-  map.logout "/logout", :controller => "sessions", :action => "destroy"
+  map.login '/login', :controller => "sessions", :action => "new"
+  map.logout '/logout', :controller => "sessions", :action => "destroy"
+  map.about '/about', :controller => 'welcome', :action => 'about' 
   map.root :controller => "welcome"
 end
