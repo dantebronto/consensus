451679b3effdce4044bf18b0b15f997fec9c244f
Kellen Presley <kellenpresley@kellen-presleys-macbook-pro.local>
2009-08-22 22:55:44 -0400

    mostly styles, layout and menu changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,20 +3,19 @@ module ApplicationHelper
     retval = ""
     for name in [:notice, :warning, :message, :error]
       if flash[name]
-       retval += "<div class=\"flash\ flash_#{name}\"><h3>#{name.to_s.titleize}:</h3><p>#{flash[name]}</p></div>"
+       retval += "<div class=\"flash\ flash_#{name}\"><h3>#{name.to_s.titleize}:</h3><span>#{flash[name]}</span></div>"
       end
     end
     retval
   end
-
+  
   def render_login_logout
-    res = "<div id='login-logout'>"
-    link = logged_in? 
-    if logged_in?
-      res += "<a href='/logout'>Logout</a>"
-    else
-      res += "<a href='/login'>Login</a>"
-    end
-    res += "</div>"
+    res = ""
+    in_or_out = logged_in? ? 'logout' : 'login'
+
+    res += "<a href='/#{in_or_out}'>"
+    res += "<span>my session</span><br />"
+    res += in_or_out
+    res += "</a>"
   end
 end
