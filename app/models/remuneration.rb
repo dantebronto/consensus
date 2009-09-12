f9bd5aae3d489d99c6a17dae6e77d60ad3c118af
Kellen Presley <kellenpresley@kellen-presleys-macbook-pro.local>
2009-09-11 20:14:41 -0400

    remuneration show action brought in line with others

--- app/models/remuneration.rb
+++ app/models/remuneration.rb
@@ -2,7 +2,7 @@ class Remuneration < ActiveRecord::Base
   CATEGORIES = ["tenure", "peer_review", "hours", "worker_misc", "org_misc", "worker_capital", "org_capital"]
   
   CATEGORIES.each do |cat|
-    define_method("#{cat}_value".to_sym) do # Renumeration.new.tenure_value => (percentage tenure * total profits)
+    define_method("#{cat}_value".to_sym) do # Renumeration.new.tenure_value => (tenure % * total_profits)
       ((self.send(cat).to_f / 100) * total_profits.to_f).to_i
     end
   end
